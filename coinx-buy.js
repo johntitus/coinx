f0c6a7663e66b4f8a1ab681bf394bcc3dc6855bc
Alex Mironov <alexandrfox@gmail.com>
2017-07-15 12:58:54 +0200

    1) Configuration rewrite: new API can be added without any changes inside coinx-*command*.js files 2) Re-tabbed files

--- coinx-buy.js
+++ coinx-buy.js
@@ -1,213 +1,163 @@
 'use strict';
 
+const coinx = require('./coinx-core');
 const path = require('path');
-const fs = require('fs');
 const program = require('commander');
 const chalk = require('chalk');
-const homedir = require('homedir');
 const capitalize = require('capitalize');
 const inquirer = require('inquirer');
 const columnify = require('columnify');
-
-const Poloniex = require('./lib/poloniex');
-const Liqui = require('./lib/liqui');
-const Bittrex = require('./lib/bittrex');
-const Bitfinex = require('./lib/bitfinex');
-const Kraken = require('./lib/kraken');
-
 const cryptocompare = require('./lib/cryptocompare');
+const config = coinx.config();
+const exchanges = coinx.exchanges();
+const coins = coinx.coins();
+let exchangesToRequest = [];
 
-const coinxHome = path.join(homedir(), 'coinx');
-const keyFilePath = path.join(coinxHome, 'coinx.json');
-const coinListPath = path.join(coinxHome, 'coinList.json');
-
-let configured = true;
-if (!fs.existsSync(keyFilePath)) showNotConfigured();
-
-let keys = require(keyFilePath);
-if (!Object.keys(keys).length) showNotConfigured();
-
-let coins = {};
-try {
-	coins = require(coinListPath);
-} catch (e) {
-	console.error(chalk.red('Please run `coinx update` to get the latest list of coins.'));
-	process.exit(1);
+if (Object.keys(coins).length === 0) {
+    console.error(chalk.red('Please run `coinx update` to get the latest list of coins.'));
+    process.exit(1);
 }
 
 program
-	.option('-$, --usd [amount]', 'Amount of US Dollars to spend.')
-	.option('-e, --exchange [name]', 'A specific exchange to buy from.')
-	.parse(process.argv);
+    .option('-$, --usd [amount]', 'Amount of US Dollars to spend.')
+    .option('-e, --exchange [name]', 'A specific exchange to buy from.')
+    .parse(process.argv);
+
+if (Object.keys(config).length === 0) {
+    console.log(chalk.red('Need to configure at least one exchange.'));
+    console.log(chalk.red('Run \'coinx configure [name of exchange]\''));
+    process.exit(1);
+}
 
 let symbol = program.args;
-
-if (!symbol.length) {
-	console.error(chalk.red('No coin symbol provided.'));
-	process.exit(1);
+if (! symbol.length) {
+    console.error(chalk.red('No coin symbol provided.'));
+    process.exit(1);
 }
 
 symbol = symbol[0].toUpperCase();
-
-if (!program.usd) {
-	console.log(chalk.red('You must specify the amount of USD to spend. -$ or --usd'));
+if (! program.usd) {
+    console.log(chalk.red('You must specify the amount of USD to spend. -$ or --usd'));
 }
 
-let validExchanges = [
-	'poloniex',
-	'bittrex',
-	'liqui',
-	'kraken',
-	'bitfinex'
-];
-
-let exchanges = [];
-
 if (program.exchange) {
-	if (validExchanges.indexOf(program.exchange) == -1) {
-		console.log(chalk.red('Unknown exchange'));
-		process.exit(1);
-	}
-	if (program.exchange == 'poloniex' && keys['poloniex']) exchanges.push(new Poloniex(keys['poloniex'].apiKey, keys['poloniex'].apiSecret));
-	if (program.exchange == 'kraken' && keys['kraken']) exchanges.push(new Kraken(keys['kraken'].apiKey, keys['kraken'].apiSecret));
-	if (program.exchange == 'liqui' && keys['liqui']) exchanges.push(new Liqui(keys['liqui'].apiKey, keys['liqui'].apiSecret));
-	if (program.exchange == 'bittrex' && keys['bittrex']) exchanges.push(new Bittrex(keys['bittrex'].apiKey, keys['bittrex'].apiSecret));
-	if (program.exchange == 'bitfinex' && keys['bitfinex']) exchanges.push(new Bitfinex(keys['bitfinex'].apiKey, keys['bitfinex'].apiSecret));
+    if (Object.keys(exchanges).indexOf(program.exchange) === -1){
+        console.log(chalk.red('Unknown exchange'));
+        process.exit(1);
+    }
+    if (Object.keys(config).indexOf(program.exchange) === -1) {
+        console.log(chalk.red('Exchange is not configured'));
+        process.exit(1);
+    }
+    exchangesToRequest.push(exchanges[program.exchange]);
+    console.log(chalk.blue('Buying on ' + capitalize(program.exchange) + '...'));
 } else {
-	if (keys['poloniex']) exchanges.push(new Poloniex(keys['poloniex'].apiKey, keys['poloniex'].apiSecret));
-	if (keys['kraken']) exchanges.push(new Kraken(keys['kraken'].apiKey, keys['kraken'].apiSecret));
-	if (keys['liqui']) exchanges.push(new Liqui(keys['liqui'].apiKey, keys['liqui'].apiSecret));
-	if (keys['bittrex']) exchanges.push(new Bittrex(keys['bittrex'].apiKey, keys['bittrex'].apiSecret));
-	if (keys['bitfinex']) exchanges.push(new Bitfinex(keys['bitfinex'].apiKey, keys['bitfinex'].apiSecret));
+    for (const name in config) {
+        exchangesToRequest.push(exchanges[name]);
+    }
+    console.log(chalk.blue('Buying...'));
 }
 
 if (program.usd) {
-	Promise.all([
-			getBTCPriceInUSD(),
-			getCoinPriceInBTC(symbol)
-		])
-		.then(results => {
-			let btcPrice = results[0];
-			let coinPrices = results[1];
+    Promise.all([
+            getBTCPriceInUSD(),
+            getCoinPriceInBTC(exchangesToRequest, symbol)
+        ])
+        .then(results => {
+            let btcPrice = results[0];
+            let coinPrices = results[1];
 
-			coinPrices.map(result => {
-				result.priceUSD = (result.priceBTC * btcPrice).toFixed(2);
-				return result;
-			});
+            coinPrices.map(result => {
+                result.priceUSD = (result.priceBTC * btcPrice).toFixed(2);
+                return result;
+            });
 
-			if (!coinPrices.length) {
-				console.log(chalk.red('Coin not found on any exchange.'));
-				process.exit(0);
-			}
+            if (!coinPrices.length) {
+                console.log(chalk.red('Coin not found on any exchange.'));
+                process.exit(0);
+            }
 
-			coinPrices.sort((a, b) => {
-				if (a.priceBTC < b.priceBTC) return -1;
-				return 1;
-			});
+            coinPrices.sort((a, b) => {
+                if (a.priceBTC < b.priceBTC) return -1;
+                return 1;
+            });
 
-			let bestMarket = coinPrices.shift();
+            let bestMarket = coinPrices.shift();
 
-			console.log(chalk.green('Best price found on ' + capitalize(bestMarket.exchange) + ' at $' + bestMarket.priceUSD));
+            if (bestMarket.exchange in config) {
+                console.log(chalk.green('Best price found on ' + capitalize(bestMarket.exchange) + ' at $' + bestMarket.priceUSD));
+            } else {
+                console.log(chalk.red('Best price found on ' + capitalize(bestMarket.exchange) + ' but it is not configured, run "coinx config ' + bestMarket.exchange + '" to configure'));
+                process.exit(1);
+            }
+            let numCoinsToBuy = (program.usd / (bestMarket.priceBTC * btcPrice)).toFixed(8);
 
-			let numCoinsToBuy = (program.usd / (bestMarket.priceBTC * btcPrice)).toFixed(8);
+            console.log('');
+            console.log(chalk.magenta('*Note that the number of coins may change slightly if the market fluctuates*'));
+            console.log('');
 
-			console.log('');
-			console.log(chalk.magenta('*Note that the number of coins may change slightly if the market fluctuates*'));
-			console.log('');
+            let questions = [{
+                type: 'confirm',
+                name: 'proceed',
+                message: 'Buy about ' + numCoinsToBuy + ' worth of ' + symbol + '?'
+            }];
 
-			let questions = [{
-				type: 'confirm',
-				name: 'proceed',
-				message: 'Buy about ' + numCoinsToBuy + ' worth of ' + symbol + '?'
-			}];
-
-			inquirer
-				.prompt(questions)
-				.then(results => {
-					if (!results.proceed) {
-						process.exit(0);
-					}
-
-					console.log(chalk.green('Buying...'));
-
-					// this is dumb. figure out a better way.
-					let exchange;
-					switch (bestMarket.exchange){
-						case 'poloniex': {
-							exchange = new Poloniex(keys['poloniex'].apiKey, keys['poloniex'].apiSecret);
-							break;
-						}
-						case 'kraken': {
-							exchange = new Kraken(keys['kraken'].apiKey, keys['kraken'].apiSecret);
-							break;
-						}
-						case 'bitfinex': {
-							exchange = new Bitfinex(keys['bitfinex'].apiKey, keys['bitfinex'].apiSecret);
-							break;
-						}
-						case 'bittrex': {
-							exchange = new Bittrex(keys['bittrex'].apiKey, keys['bittrex'].apiSecret);
-							break;
-						}
-						case 'liqui': {
-							exchange = new Liqui(keys['liqui'].apiKey, keys['liqui'].apiSecret);
-							break;
-						}
-					}
-					return exchange.buy(symbol, program.usd);
-				})
-				.then( result => {
-					if (result.complete){
-						console.log(chalk.green('Order complete!'));
-						console.log(chalk.green(capitalize(result.market) + ' order number ' + result.orderNumber));
-						console.log(chalk.green('Bought ' + result.numCoinsBought + ' ' + symbol + ' at ' + result.rate + ' BTC per coin'));
-						console.log(chalk.green('Worth about $' + parseFloat(result.usdValue).toFixed(2) ));
-					} else {
-						console.log(chalk.green('Order placed, but not completed.'));
-						console.log(chalk.green(capitalize(result.market) + ' order number ' + result.orderNumber));
-						console.log('Details:');
-						console.log(result);
-					}
-				})
-				.catch( e => {
-					console.error(chalk.red('An error occurred.'));
-					console.log(e);
-				});
-		});
+            inquirer
+                .prompt(questions)
+                .then(results => {
+                    if (!results.proceed) {
+                        process.exit(0);
+                    }
+                    console.log(chalk.green('Buying...'));
+                    return exchanges[bestMarket.exchange].buy(symbol, program.usd);
+                })
+                .then( result => {
+                    if (result.complete){
+                        console.log(chalk.green('Order complete!'));
+                        console.log(chalk.green(capitalize(result.market) + ' order number ' + result.orderNumber));
+                        console.log(chalk.green('Bought ' + result.numCoinsBought + ' ' + symbol + ' at ' + result.rate + ' BTC per coin'));
+                        console.log(chalk.green('Worth about $' + parseFloat(result.usdValue).toFixed(2) ));
+                    } else {
+                        console.log(chalk.green('Order placed, but not completed.'));
+                        console.log(chalk.green(capitalize(result.market) + ' order number ' + result.orderNumber));
+                        console.log('Details:');
+                        console.log(result);
+                    }
+                })
+                .catch( e => {
+                    console.error(chalk.red('An error occurred.'));
+                    console.log(e);
+                });
+        });
 }
 
-function getCoinPriceInBTC(symbol) {
-	if (coins[symbol]){
-		console.log(chalk.blue('Checking ' + coins[symbol].name + ' (' + symbol + ') on the markets...'));
-	} else {
-		console.log(chalk.blue('Checking ' + symbol + ' on the markets...'));
-	}
-	
-	let coinPriceRequests = exchanges.map(exchange => {
-		return exchange.getPriceInBTC(symbol).catch(e => {
-			console.log('error')
-			console.log(e);
-			console.log(exchange)
-		});
-	});
+function getCoinPriceInBTC(exchanges, symbol) {
+    if (coins[symbol]){
+        console.log(chalk.blue('Checking ' + coins[symbol].name + ' (' + symbol + ') on the markets...'));
+    } else {
+        console.log(chalk.blue('Checking ' + symbol + ' on the markets...'));
+    }
 
-	return Promise
-		.all(coinPriceRequests)
-		.then(results => {
-			let priceResults = results.filter(result => {
-				return result.available && result.priceBTC;
-			});
+    let coinPriceRequests = exchanges.map(exchange => {
+        return exchange.getPriceInBTC(symbol).catch(e => {
+            console.log('error')
+            console.log(e);
+            console.log(exchange)
+        });
+    });
 
-			return priceResults;
-		});
+    return Promise
+        .all(coinPriceRequests)
+        .then(results => {
+            let priceResults = results.filter(result => {
+                return result.available && result.priceBTC;
+            });
+
+            return priceResults;
+        });
 }
 
 function getBTCPriceInUSD() {
-	return cryptocompare.price('BTC','USD');
+    return cryptocompare.price('BTC','USD');
 }
 
-function showNotConfigured() {
-	console.log(chalk.red('Need to configure at least one exchange.'));
-	console.log(chalk.red('Run \'coinx config [name of exchange]\''));
-	process.exit(1);
-}
--- coinx-price.js
+++ coinx-price.js
@@ -1,205 +1,184 @@
 'use strict';
 
+const coinx = require('./coinx-core');
 const program = require('commander');
 const chalk = require('chalk');
 const capitalize = require('capitalize');
 const columnify = require('columnify');
-const path = require('path');
-const homedir = require('homedir');
-
-const Poloniex = require('./lib/poloniex');
-const Liqui = require('./lib/liqui');
-const Bittrex = require('./lib/bittrex');
-const Bitfinex = require('./lib/bitfinex');
-const Kraken = require('./lib/kraken');
-
 const cryptocompare = require('./lib/cryptocompare');
+const coins = coinx.coins();
+const exchanges = Object.values(coinx.exchanges());
 
-const coinxHome = path.join(homedir(), 'coinx');
-const coinListPath = path.join(coinxHome, 'coinList.json');
-
-let coins = {};
-try {
-	coins = require(coinListPath);
-} catch (e) {
-	console.error(chalk.red('Please run `coinx update` to get the latest list of coins.'));
-	process.exit(1);
+if (Object.keys(coins).length === 0) {
+    console.error(chalk.red('Please run `coinx update` to get the latest list of coins.'));
+    process.exit(1);
 }
-
-let exchanges = [
-	new Poloniex(),
-	new Liqui(),
-	new Bittrex(),
-	new Bitfinex(),
-	new Kraken()
-];
 
 program.parse(process.argv);
 
 var symbol = program.args;
 
 if (!symbol.length) {
-	console.error(chalk.red('No coin symbol provided.'));
-	process.exit(1);
+    console.error(chalk.red('No coin symbol provided.'));
+    process.exit(1);
 }
 
 symbol = symbol[0].toUpperCase();
 
 if (coins[symbol]){
-	console.log(chalk.blue('Getting prices for ' + coins[symbol].name + ' (' + symbol + ')...'));
+    console.log(chalk.blue('Getting prices for ' + coins[symbol].name + ' (' + symbol + ')...'));
 } else {
-	console.log(chalk.blue('Getting prices for ' + symbol + '...'));
+    console.log(chalk.blue('Getting prices for ' + symbol + '...'));
 }
 
 
 let requests = [];
 
 if (symbol == 'BTC'){
-	requests = exchanges.map( exchange => {
-		return exchange.getBTCinUSD()
-	});
+    requests = exchanges.map( exchange => {
+        return exchange.getBTCinUSD()
+    });
 } else {
-	requests = [
-		cryptocompare.price('BTC','USD')
-	];
-	let priceInBTCRequests = exchanges.map( exchange => {
-		return exchange.getPriceInBTC(symbol);
-	});
-	requests = requests.concat(priceInBTCRequests);
+    requests = [
+        cryptocompare.price('BTC','USD')
+    ];
+    let priceInBTCRequests = exchanges.map( exchange => {
+        return exchange.getPriceInBTC(symbol);
+    });
+    requests = requests.concat(priceInBTCRequests);
 }
 
 Promise
-	.all(requests)
-	.then(results => {
-		if (symbol == 'BTC'){
-			processBTC(results);
-		} else {
-			processCoin(results);
-		}
-	});
+    .all(requests)
+    .then(results => {
+        if (symbol == 'BTC'){
+            processBTC(results);
+        } else {
+            processCoin(results);
+        }
+    });
 
 function processBTC(results){
-	let priceResults = results.filter(result => {
-		return result.available;
-	});
-	if (!priceResults.length) {
-		console.log(chalk.red('Coin not found on any exchange.'));
-		process.exit(0);
-	}
+    let priceResults = results.filter(result => {
+        return result.available;
+    });
+    if (!priceResults.length) {
+        console.log(chalk.red('Coin not found on any exchange.'));
+        process.exit(0);
+    }
 
-	let averageUSD = priceResults.reduce((sum, result) => {
-		return parseFloat(sum) + parseFloat(result.priceUSD);
-	}, 0.0) / priceResults.length;
+    let averageUSD = priceResults.reduce((sum, result) => {
+        return parseFloat(sum) + parseFloat(result.priceUSD);
+    }, 0.0) / priceResults.length;
 
-	priceResults.sort( (a, b) => {
-		if (a.priceUSD < b.priceUSD){
-			return -1;
-		}
-		return 1;
-	});
+    priceResults.sort( (a, b) => {
+        if (a.priceUSD < b.priceUSD){
+            return -1;
+        }
+        return 1;
+    });
 
-	priceResults.push({});
+    priceResults.push({});
 
-	priceResults.push({
-		exchange: 'average',
-		priceUSD: averageUSD
-	});
+    priceResults.push({
+        exchange: 'average',
+        priceUSD: averageUSD
+    });
 
-	let columns = columnify(priceResults, {
-		columns: ['exchange', 'priceUSD'],
-		config: {
-			exchange: {
-				headingTransform: function(heading) {
-					return capitalize(heading);
-				},
-				dataTransform: function(data) {
-					return capitalize(data);
-				}
-			},
-			
-			priceUSD: {
-				headingTransform: function(heading) {
-					return 'Price in USD'
-				},
-				dataTransform: function(data) {
-					return (data) ? '$' + parseFloat(data).toFixed(2) : '';
-				},
-				align: 'right'
-			}
-		}
-	});
-	console.log(columns);
+    let columns = columnify(priceResults, {
+        columns: ['exchange', 'priceUSD'],
+        config: {
+            exchange: {
+                headingTransform: function(heading) {
+                    return capitalize(heading);
+                },
+                dataTransform: function(data) {
+                    return capitalize(data);
+                }
+            },
+            priceUSD: {
+                headingTransform: function(heading) {
+                    return 'Price in USD'
+                },
+                dataTransform: function(data) {
+                    return (data) ? '$' + parseFloat(data).toFixed(2) : '';
+                },
+                align: 'right'
+            }
+        }
+    });
+    console.log(columns);
 }
 
 
 function processCoin(results){
-	let btcPrice = results.shift();
+    let btcPrice = results.shift();
 
-	let priceResults = results.filter(result => {
-		return result.available && result.priceBTC;
-	}).map(result => {
-		result.priceUSD = (result.priceBTC * btcPrice).toFixed(3);
-		return result;
-	});
+    let priceResults = results.filter(result => {
+        return result.available && result.priceBTC;
+    }).map(result => {
+        result.priceUSD = (result.priceBTC * btcPrice).toFixed(3);
+        return result;
+    });
 
-	if (!priceResults.length) {
-		console.log(chalk.red('Coin not found on any exchange.'));
-		process.exit(0);
-	}
+    if (!priceResults.length) {
+        console.log(chalk.red('Coin not found on any exchange.'));
+        process.exit(0);
+    }
 
-	let averageUSD = priceResults.reduce((sum, result) => {
-		return parseFloat(sum) + parseFloat(result.priceUSD);
-	}, 0.0) / priceResults.length;
+    let averageUSD = priceResults.reduce((sum, result) => {
+        return parseFloat(sum) + parseFloat(result.priceUSD);
+    }, 0.0) / priceResults.length;
 
-	let averageBTC = priceResults.reduce((sum, result) => {
-		return parseFloat(sum) + parseFloat(result.priceBTC);
-	}, 0) / priceResults.length;
+    let averageBTC = priceResults.reduce((sum, result) => {
+        return parseFloat(sum) + parseFloat(result.priceBTC);
+    }, 0) / priceResults.length;
 
-	priceResults.sort( (a, b) => {
-		if (a.priceUSD < b.priceUSD){
-			return -1;
-		}
-		return 1;
-	});
+    priceResults.sort( (a, b) => {
+        if (a.priceUSD < b.priceUSD){
+            return -1;
+        }
+        return 1;
+    });
 
-	priceResults.push({});
+    priceResults.push({});
 
-	priceResults.push({
-		exchange: 'average',
-		priceBTC: averageBTC,
-		priceUSD: averageUSD
-	});
+    priceResults.push({
+        exchange: 'average',
+        priceBTC: averageBTC,
+        priceUSD: averageUSD
+    });
 
-	let columns = columnify(priceResults, {
-		columns: ['exchange', 'priceBTC', 'priceUSD'],
-		config: {
-			exchange: {
-				headingTransform: function(heading) {
-					return capitalize(heading);
-				},
-				dataTransform: function(data) {
-					return capitalize(data);
-				}
-			},
-			priceBTC: {
-				headingTransform: function(heading) {
-					return 'Price in BTC'
-				},
-				dataTransform: function(data) {
-					return (data) ? parseFloat(data).toFixed(8) : '';
-				},
-				align: 'right'
-			},
-			priceUSD: {
-				headingTransform: function(heading) {
-					return 'Price in USD'
-				},
-				dataTransform: function(data) {
-					return (data) ? '$' + parseFloat(data).toFixed(2) : '';
-				},
-				align: 'right'
-			}
-		}
-	});
-	console.log(columns);
+    let columns = columnify(priceResults, {
+        columns: ['exchange', 'priceBTC', 'priceUSD'],
+        config: {
+            exchange: {
+                headingTransform: function(heading) {
+                    return capitalize(heading);
+                },
+                dataTransform: function(data) {
+                    return capitalize(data);
+                }
+            },
+            priceBTC: {
+                headingTransform: function(heading) {
+                    return 'Price in BTC'
+                },
+                dataTransform: function(data) {
+                    return (data) ? parseFloat(data).toFixed(8) : '';
+                },
+                align: 'right'
+            },
+            priceUSD: {
+                headingTransform: function(heading) {
+                    return 'Price in USD'
+                },
+                dataTransform: function(data) {
+                    return (data) ? '$' + parseFloat(data).toFixed(2) : '';
+                },
+                align: 'right'
+            }
+        }
+    });
+    console.log(columns);
 }
